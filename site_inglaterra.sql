4edbcc3b5c055bd0362ab3d0962953a5963770e7
Marilia Godoy <mariliapn@gmail.com>
2024-06-17 11:58:52 -0300

    Banco de dados

--- site_inglaterra.sql
+++ site_inglaterra.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 13/06/2024 às 21:37
+-- Tempo de geração: 17/06/2024 às 01:01
 -- Versão do servidor: 10.4.28-MariaDB
--- Versão do PHP: 8.0.28
+-- Versão do PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -40,8 +40,53 @@ CREATE TABLE `comentarios` (
 --
 
 INSERT INTO `comentarios` (`id`, `nome`, `email`, `comentario`, `data`) VALUES
-(1, 'Marilia', 'marilia@email.com', 'Adoreiii', '2024-06-13 18:34:59'),
-(2, 'Davi Godoy', 'davi@email.com', 'Vou pelo menos uma vez por anos para Inglaterra!!', '2024-06-13 18:50:51');
+(5, 'Doni', 'doni@email.com', 'Adoreiiiiiii', '2024-06-14 23:43:15'),
+(6, 'Alan', 'alan@email.com', 'coloquei fogo no parquinho na Inglaterra', '2024-06-14 23:45:53');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `comentarios_aprovados`
+--
+
+CREATE TABLE `comentarios_aprovados` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `comentario` text NOT NULL,
+  `data` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `comentarios_aprovados`
+--
+
+INSERT INTO `comentarios_aprovados` (`id`, `nome`, `email`, `comentario`, `data`) VALUES
+(1, 'Davi Godoy', 'davi@email.com', 'Vou pelo menos uma vez por anos para Inglaterra!!', '2024-06-15 20:25:12'),
+(2, 'Davi', 'davi@email.com', 'É o pais mais lindo do mundo! Amo demais', '2024-06-16 18:54:51');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios_admin`
+--
+
+CREATE TABLE `usuarios_admin` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `usuario` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `data` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuarios_admin`
+--
+
+INSERT INTO `usuarios_admin` (`id`, `nome`, `usuario`, `senha`, `data`) VALUES
+(2, 'Administrador', 'admin', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', '2024-06-15 20:16:10'),
+(3, 'Marilia', 'marilia', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', '2024-06-15 21:31:11'),
+(4, 'Marilia', 'marilia', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', '2024-06-15 21:49:40');
 
 --
 -- Índices para tabelas despejadas
@@ -54,6 +99,18 @@ ALTER TABLE `comentarios`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices de tabela `comentarios_aprovados`
+--
+ALTER TABLE `comentarios_aprovados`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `usuarios_admin`
+--
+ALTER TABLE `usuarios_admin`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT para tabelas despejadas
 --
 
@@ -61,6 +118,18 @@ ALTER TABLE `comentarios`
 -- AUTO_INCREMENT de tabela `comentarios`
 --
 ALTER TABLE `comentarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de tabela `comentarios_aprovados`
+--
+ALTER TABLE `comentarios_aprovados`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios_admin`
+--
+ALTER TABLE `usuarios_admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
